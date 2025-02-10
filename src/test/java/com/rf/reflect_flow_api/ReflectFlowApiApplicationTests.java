4406d1417631eca64df125e534efdee8831a6ada
marian-glt <38663778+marian-glt@users.noreply.github.com>
2025-02-10 15:04:22 +0000

    Git: initial commit

---
+++ src/test/java/com/rf/reflect_flow_api/ReflectFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rf.reflect_flow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectFlowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
